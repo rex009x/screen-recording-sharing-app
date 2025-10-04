2b4f2e9b58dc2b39516c0825a84c267b03627d86
Jimmy Luc <jimmyluc15910@gmail.com>
2025-10-03 20:11:56 -0400

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
+import { NextRequest, NextResponse, NextFetchEvent } from "next/server";
 import { auth } from "@/lib/auth";
 import aj from "./lib/arcjet";
 import { createMiddleware, detectBot, shield } from "@arcjet/next";
+
+export const config = {
+  runtime: "nodejs",
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
+};
 
 const ajmiddleware = createMiddleware(
   aj
@@ -14,30 +18,30 @@ const ajmiddleware = createMiddleware(
     .withRule(
       detectBot({
         mode: "LIVE",
-        allow: ["CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER"], // allow other bots if you want to.
+        allow: ["CATEGORY:SEARCH_ENGINE", "G00G1E_CRAWLER", "CATEGORY:PREVIEW"],
       })
     )
 );
 
-export async function middleware(request: NextRequest) {
-  // Run Arcjet middleware first
-  const arcjetResponse = await ajmiddleware(request);
-  if (arcjetResponse) {
-    return arcjetResponse;
+export default async function middleware(
+  request: NextRequest,
+  event: NextFetchEvent
+) {
+  const arcjetResponse = await ajmiddleware(request, event);
+  if (arcjetResponse?.status === 403) {
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
   }
 
-  // Then run your auth logic
+  if (arcjetResponse?.status === 429) {
+    return NextResponse.json({ error: "Too Many Requests" }, { status: 429 });
+  }
+
   const session = await auth.api.getSession({
     headers: request.headers,
   });
 
-  if (!session) {
+  if (!session?.user) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
-
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
-};
